e486d39bc2d0f12c62a99a93398e8d157c03aca7
codebendr-product-designer <codebendr@gmail.comm>
2021-02-14 23:13:29 +0000

    fixed package syntax error

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "EssentialFeedSDK",
-    platforms: [.iOS(.v12), .macOS("11.0")]
+    platforms: [.iOS(.v12), .macOS("11.0")],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
